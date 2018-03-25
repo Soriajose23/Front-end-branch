7eb8da9d9d07a885d76bf53680b2cc060af14756
jsoria53 <soriajose53@gmail.com>
2018-03-24 22:54:36 -0400

    fixed bar chart

--- src/app/bar-cmp/bar-cmp.component.ts
+++ src/app/bar-cmp/bar-cmp.component.ts
@@ -1,49 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 @Component({
   selector: 'app-bar-cmp',
   templateUrl: './bar-cmp.component.html',
   styleUrls: ['./bar-cmp.component.css']
 })
 export class BarCmpComponent implements OnInit {
+    stuff;
+    httpdata;
+    data;
   width = 600;
     height = 400;
     type = 'column2d';
     dataFormat = 'json';
     dataSource;
 
-  constructor() {
-    this.dataSource = this.dataSource = {
-      "chart": {
-          "caption": "Animal Popularity",
-          "subCaption": "Top 5 popular animals this year",
-        //   "numberprefix": "$",
-          "theme": "fint"
-      },
-      "data": [
-          {
-              "label": "Lion",
-              "value": "10"
+    constructor(private http: Http) {
+        const headers = new Headers();
+        headers.append('Content-Type', "application/json");
+        headers.append('Authorization', 'Bearer'+ ' '+localStorage.getItem("token"));
+        const options = new RequestOptions({headers: headers});
+        var body = ' '
+        this.http.post('https://zootropolis.herokuapp.com/Animal/topfive',body,options).
+        map (
+          (response) => response.text()
+        ).subscribe (
+          (data) => {this.display(data)}
+        )
+    
+    
+    
+        this.dataSource = this.dataSource = {
+          "chart": {
+              "caption": "Animal Popularity",
+              "subCaption": "Top 5 popular animals this year",
+            //   "numberprefix": "$",
+              "theme": "fint"
           },
-          {
-              "label": "Panda",
-              "value": "5"
-          },
-          {
-              "label": "American Bison",
-              "value": "3"
-          },
-          {
-              "label": "Alligators",
-              "value": "9"
-          },
-          {
-              "label": "Apes",
-              "value": "20"
-          }
-      ]
-  }
-   }
+          "data": this.stuff
+      }
+       }
+    
+       display(data){
+        
+        var i = 0;
+        this.httpdata = JSON.parse(data)
+        console.log(this.httpdata)
+       for (var key in this.httpdata) {
+             if (this.httpdata.hasOwnProperty(key)) {
+           var val = this.httpdata[key];
+           this.data ={label: val["animalName"], value: val["tracking"].toString()};
+           this.stuff.push(this.data)
+         }
+        
+       }
+    }
 
   ngOnInit() {
   }
