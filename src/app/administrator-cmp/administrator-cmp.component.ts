d26be5d7a21f77e4a63d49985980d2c17dc66d8b
jsoria53 <soriajose53@gmail.com>
2018-03-24 22:07:23 -0400

    cleaning

--- src/app/administrator-cmp/administrator-cmp.component.ts
+++ src/app/administrator-cmp/administrator-cmp.component.ts
@@ -47,13 +47,6 @@ export class AdministratorCmpComponent implements OnInit {
 
   ngOnInit() {
      this.zookeeper = new Zookeeper( localStorage.getItem("Username"), localStorage.getItem("Firstname"), localStorage.getItem("Lastname"), localStorage.getItem("Role"));
-    //dummydata
-    this.animals.push(new Animal(1, "M", "Bear", "Yogi", "Eats way too much", "anything" ));
-    this.animals.push(new Animal(2, "M", "Orangutan", "Dunston", "mischevious", "bananas"));
-    this.animals.push(new Animal(3, "F", "Wolf", "Akeelah", "Alpha", "red meat"));
-
-    this.inventory.push({id: 1, item: "Bananas", quantity: "50Kg", next: "2018-03-31", notes: ""});
-    this.inventory.push({id: 1, item: "Red Meat", quantity: "232Kg", next: "2018-04-06", notes: "store quickly"});
 
   }
 
